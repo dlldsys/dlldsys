33aae079bc60a408b73dac2941b28636e5bef891
dong.liang <dong.liang>
2024-12-27 14:45:43 +0800

    fix:test node 20

--- blog-group/src/app/app.component.ts
+++ blog-group/src/app/app.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { NzFloatButtonModule } from 'ng-zorro-antd/float-button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule],
+  imports: [FormsModule, RouterLink, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule,NzFloatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   isCollapsed = false;
+  isOpen: boolean = false;
+
+  openFloatButton(){
+    this.isOpen = !this.isOpen;
+  }
 }
