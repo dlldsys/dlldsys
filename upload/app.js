7d36cca2d14eac3dfb2eebde47d047eeb3a69bfa
dong.liang <dong.liang>
2025-01-21 15:15:59 +0800

    fix:bug

--- upload/app.js
+++ upload/app.js
@@ -70,3 +70,37 @@ app.get("/clear",async (req, res) => {
     res.setHeader('content-type','application/json')
     res.send("Files uploaded successfully!");
 });
+
+app.get("/ip",async (req, res) => {
+    res.setHeader('content-type','application/json')
+    res.send(JSON.stringify({ip:getLocalIP()}));
+});
+function getLocalIP() {
+    const os = require('os');
+    const osType = os.type(); //系统类型
+    const netInfo = os.networkInterfaces(); //网络信息
+    let ip = '';
+    if (osType === 'Windows_NT') { 
+        for (let dev in netInfo) {
+        	//win7的网络信息中显示为本地连接，win10显示为以太网
+            if (dev === '本地连接' || dev === '以太网'||dev === 'WLAN') {
+                for (let j = 0; j < netInfo[dev].length; j++) {
+                    if (netInfo[dev][j].family === 'IPv4') {
+                        ip = netInfo[dev][j].address;
+                        break;
+                    }
+                }
+            }
+        }
+
+    } else if (osType === 'Linux') {
+        ip = netInfo.eth0[0].address;
+    } else if (osType === 'Darwin') {
+    	// mac操作系统
+    	// ip = netInfo.eth0[0].address;
+    } else {
+    	// 其他操作系统
+    }
+
+    return ip;
+}
